47909ceaa97323fc2d611c31ae9508c0e149440b
SamNewhouse <hello@samnewhouse.co.uk>
2025-10-17 16:53:24 +0100

    Env rename

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,9 @@
 import { httpRequest } from "../core/http";
-import { EXPO_PUBLIC_EXTERNAL_TUNNEL_URL } from "../core/variables";
+import { EXPO_PUBLIC_TUNNEL_URL } from "../core/variables";
 
 export function signupAPI(email: string, password: string) {
   return httpRequest({
-    url: `${EXPO_PUBLIC_EXTERNAL_TUNNEL_URL}/dev/auth/signup`,
+    url: `${EXPO_PUBLIC_TUNNEL_URL}/dev/auth/signup`,
     method: "post",
     data: { email, password },
   });
@@ -11,7 +11,7 @@ export function signupAPI(email: string, password: string) {
 
 export function loginAPI(email: string, password: string) {
   return httpRequest({
-    url: `${EXPO_PUBLIC_EXTERNAL_TUNNEL_URL}/dev/auth/login`,
+    url: `${EXPO_PUBLIC_TUNNEL_URL}/dev/auth/login`,
     method: "post",
     data: { email, password },
   });
