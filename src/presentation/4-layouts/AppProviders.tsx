a1c0fd09451e35e9f2df5e94a32336649decb5c4
SamNewhouse <hello@samnewhouse.co.uk>
2025-10-18 22:15:30 +0100

    Removed AppProvider layout

--- src/presentation/4-layouts/AppProviders.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { GameProvider } from "../../context/GameContext";
-
-export default function AppProviders({ children }: { children: React.ReactNode }) {
-  return <GameProvider>{children}</GameProvider>;
-}
